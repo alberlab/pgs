86adfbb25d4d7bbc421c9fd71cdab5194327f60f
shanjunUSC <shanjun@usc.edu>
2016-09-09 12:21:51 -0700

    probMat test and rawMat test script added

---
+++ test/runPgs_probMat_run_test.sh
@@ -0,0 +1,1 @@
+python ../pgs.py --input_config input_config_probMat_run_test.json --run_mode local --pyflow_dir probMat_run_test
---
+++ test/runPgs_rawMat_run_test.sh
@@ -0,0 +1,1 @@
+python ../pgs.py --input_config input_config_rawMat_run_test.json --run_mode local --pyflow_dir rawMat_run_test

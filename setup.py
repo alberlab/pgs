4753e8c8f6ea7a9fefc6722de1b581f0da332a70
shanjunUSC <shanjun@usc.edu>
2016-09-12 15:01:03 -0700

    change workflows to pgsflows. setup.py chagned.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
 	author_email = 'shanjun@usc.edu', 
 	url = '', 
 	description = '3D Modeling Pipeline Workflows',
-	packages=['alab', 'workflows', 'workflows.modeling'],
-	package_data={'alab' : ['genomes/*']}
+	packages=['alab'],
+	pckage_data={'alab' : ['genomes/*']}
 	)

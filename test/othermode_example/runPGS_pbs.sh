06d264ea8bd7c8bf03642eb81bf870165cc313af
ursinedeity <huanan1991@gmail.com>
2016-09-22 17:04:26 -0700

    change rm files

--- test/othermode_example/runPGS_pbs.sh
+++
@@ -1,1 +0,0 @@
-python $PGS_SOURCE_DIR/pgs.py --input_config $WORKING_DIR/input_config.json --run_mode pbs --nCores 300 --memMb 800000 --pyflow_dir $WORKING_DIR --schedulerArgList ['-q','YOUR_QUEUE_NAME','-l','walltime=77:00:00]
